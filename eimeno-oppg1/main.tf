021f05d14c86a0d2c154e69c3535041c6754904b
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-09-29 20:18:29 +0200

    Created the key vault module block in the root module main config file

--- eimeno-oppg1/main.tf
+++ eimeno-oppg1/main.tf
@@ -32,4 +32,19 @@ module "VirtualMachine" {
   location = var.location
   backend_key = var.vm_backend_key
   subnet_id = module.VirtualNetwork.subnet_id
+  #admin_username =
+  #vm_passwd =
+}
+
+module "KeyVault" {
+  source = "./KeyVault"
+  base_name = var.kv_base_name
+  location = var.location
+  backend_key = var.kv_backend_key
+  sa_ak_secret_name = var.sa_ak_secret_name
+  sa_primary_access_key = module.StorageAccount.sa_primary_access_key
+  vm_username_secret_name = var.vm_username_secret_name
+  vm_username = var.vm_username
+  vm_passwd_secret_name = var.vm_passwd_secret_name
+  vm_passwd = var.vm_passwd
 }
