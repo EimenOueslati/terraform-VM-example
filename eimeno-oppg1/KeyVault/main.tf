fc59fcdf49c011a7a8e0c39c3aa5e8984974aa19
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-09-29 20:14:53 +0200

    Added all the necessary variables in the variables file of the key vault module

--- eimeno-oppg1/KeyVault/main.tf
+++ eimeno-oppg1/KeyVault/main.tf
@@ -18,6 +18,9 @@ locals {
     workspaces_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
     rg_name = "KV-RG-${var.base_name}-${local.workspace_suffix}"
     location = var.location
+    key_permissions = ["Create", "List", "Get","Backup", "Decrypt", "Delete", "Encrypt", "Import", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"]
+    secret_permissions = ["Get", "List", "Set", "Backup", "Delete", "Purge", "Recover", "Restore"]
+    storage_permissions = ["Get", "List", "Set", "Backup", "Delete", "DeleteSAS", "GetSAS", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "SetSAS", "Update"]
 }
 
 resource "azurerm_resource_group" "kv-rg" {
@@ -43,33 +46,33 @@ resource "azurerm_key_vault" "kv" {
     object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
-      var.permissions[0], var.permissions[1], var.permissions[2],
+      local.key_permissions[0], local.key_permissions[1], local.key_permissions[2],
     ]
 
     secret_permissions = [
-      var.permissions[0], var.permissions[1], var.permissions[3],
+      local.secret_permissions[0], local.secret_permissions[1], local.secret_permissions[2],
     ]
 
     storage_permissions = [
-      var.permissions[0], var.permissions[1], var.permissions[3],
+        local.storage_permissions[0], local.storage_permissions[1], local.storage_permissions[2],
     ]
   }
 }
 
 resource "azurerm_key_vault_secret" "sa_accesskey" {
-  name         = var.sa_ak_name
+  name         = var.sa_ak_secret_name
   value        = var.sa_primary_access_key
   key_vault_id = azurerm_key_vault.kv.id
 }
 
 resource "azurerm_key_vault_secret" "vm_username" {
-  name         = var.vm_username_name
+  name         = var.vm_username_secret_name
   value        = var.vm_username
   key_vault_id = azurerm_key_vault.kv.id
 }
 
 resource "azurerm_key_vault_secret" "vm_passwd" {
-    name         = var.vm_passwd_name
+    name         = var.vm_passwd_secret_name
     value        = var.vm_passwd
     key_vault_id = azurerm_key_vault.kv.id
 }
---
+++ eimeno-oppg1/KeyVault/variables.tf
@@ -0,0 +1,44 @@
+variable "backend_key" {
+    type = string
+    description = "The name of the key for the backend"
+}
+
+variable "base_name" {
+    type = string
+    description = "The base name for all the resources in the module"
+}
+  
+variable "location" {
+    type = string
+    description = "The Azure region that the resources should be deployed in" 
+}
+
+variable "sa_ak_secret_name" {
+  type = string
+    description = "The name of the secret for access key of the storage account"
+}
+
+variable "sa_primary_access_key" {
+  type = string
+    description = "The primary access key for the storage account"
+}
+
+variable "vm_username_secret_name" {
+  type = string
+    description = "The name of the secret for the username of the virtual machine"
+}
+
+variable "vm_username" {
+  type = string
+    description = "The username for the virtual machine"
+}
+
+variable "vm_passwd_secret_name" {
+  type = string
+    description = "The name of the secret for the password of the virtual machine"
+}
+  
+variable "vm_passwd" {
+  type = string
+    description = "The password for the virtual machine"
+}
